4896941f3c24ffed4a38f19241c56814afa241a2
Jeremy Unanue <jrunanue@loyola.edu>
2022-07-11 12:05:00 -0400

    finished project

---
+++ src/components/ToDo.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="list">
+    
+  </div>
+</template>
+
+<script>
+  export default{
+    data() {
+      return {
+        previous: null,
+        current: '',
+        operator: null,
+        operatorClicked: false
+      }
+    },
+    methods: {
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
